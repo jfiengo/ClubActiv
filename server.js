e22a8ebf9ad3a65ee140b32e22b1a167356e6fd0
Jack Fiengo <fiengoj@gmail.com>
2024-07-10 16:49:02 -0600

    Added exit after end of testing and fixed warnings

--- server.js
+++ server.js
@@ -16,10 +16,7 @@ app.use('/api/users', usersRouter);
 app.use('/api/groups', groupsRouter);
 app.use('/api/posts', postsRouter);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.error(err));
 
